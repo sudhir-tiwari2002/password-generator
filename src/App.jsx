51571db6e7108cca00b49eff7b69178169146b5a
sudhir.pw <skt2ktiwari@gmail.com>
2023-09-13 12:22:07 +0530

    initial setup of password generator

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='flex flex-wrap items-center justify-center'>
+        <h1>this is heading</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
